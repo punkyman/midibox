513abf99ee019239a07fc94edd83d3daec90dd8d
unknown <julien.reiss@gmail.com>
2019-04-25 07:19:56 +0200

    add display library dependency, first test of display

--- midibox/src/main.cpp
+++ midibox/src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2); 
 
 void setup() {
   // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  lcd.print("prout");
 }
 
 void loop() {
